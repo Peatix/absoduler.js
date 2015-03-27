41f2aec71baf6b111e07618cee3d4da3918f1f44
Akira Sawada <akira@peatix.com>
2015-03-27 17:39:08 +0900

    Fix index.js

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 var Absoduler = module.exports = require('./lib/Absoduler');
 Absoduler.Server = require('./lib/Server');
 Absoduler.Serialize = require('./lib/Serialize');
+Absoduler.NodeAbsodulerize = require('./lib/Node');
