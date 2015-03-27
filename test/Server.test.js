2138f8406a35bcfc0424f70ad202e29c17840c81
Akira Sawada <akira@peatix.com>
2015-03-27 17:22:29 +0900

    Add fallback ( websocket transparency )

--- test/Server.test.js
+++ test/Server.test.js
@@ -2,6 +2,8 @@ var Server = require('../lib/Server')
   , Absoduler = require('../lib/Absoduler')
   , plan = require('./testutil').plan
   ;
+Absoduler.NodeAbsodulerize = require('../lib/Node')
+
 Server.Serialize = Absoduler.Serialize = require('../lib/Serialize');
 require('should');
 
@@ -42,4 +44,40 @@ describe('Absoduler.Server', function () {
       }, 1000);
     });
   });
+
+  describe('Fallback', function () {
+    context.prototype.plan = function (t){console.log(t)};
+    plan(5).it('can fallback', function (done) {
+      this.timeout(8000); // Wait synchronize
+      var server;
+      (function() {
+        server = new Server({port:8088});
+        server.on('connection', function(ws) {
+          setTimeout( function () {
+            ws.sendEvent('fire', 200);
+            done();
+          }, 2000);
+          setTimeout( function () {
+            ws.send('water');
+            done();
+          }, 2000);
+        });
+      }).should.not.throw();
+
+      setTimeout( function () {
+        cli = new Absoduler({ server: 'ws://localhost:8088/' });
+        cli.on('initialized', function () {
+          done();
+        });
+        cli.on('fire', function () {
+          done();
+        });
+        cli.on('message', function (msg) {
+          msg.should.equal('water');
+          done();
+        });
+      }, 1000);
+    });
+  });
+
 });
